ffb605568255d537e12fa2e47a0d2bc6eb5bcc15
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2024-12-20 04:13:24 +0100

    Add rarity tag to hover card

--- src/components/astro/inventory/InventoryHover.tsx
+++ src/components/astro/inventory/InventoryHover.tsx
@@ -1,9 +1,11 @@
+import { RarityTag } from "@/components/react/rarity-tag";
 import { ReactBergBalance } from "@/components/react/react-balance";
 import {
     HoverCard,
     HoverCardTrigger,
     HoverCardContent,
 } from "@/components/ui/hover-card";
+import { Rarity } from "@/gambling/enums";
 import { items } from "@/gambling/items";
 import type React from "react";
 
@@ -17,13 +19,13 @@ export const InventoryHover: React.FC<{
         <HoverCard>
             <HoverCardTrigger>{props.children}</HoverCardTrigger>
             <HoverCardContent className="w-80">
-                <div className="flex justify-between space-x-4">
-                    <div className="space-y-1">
+                <div className="flex justify-between w-full space-x-4">
+                    <div className="space-y-1 w-full">
                         <h3 className="text-lg">{it?.name || "Leerer Slot"}</h3>
                         {it?.description && (
                             <p className="text-sm">{it?.description}</p>
                         )}
-                        <div className="flex items-center pt-2">
+                        <div className="flex items-center justify-between w-full pt-2">
                             <span className="text-xs text-muted-foreground">
                                 <ReactBergBalance
                                     amount={it?.value || 0}
@@ -31,6 +33,11 @@ export const InventoryHover: React.FC<{
                                     className="text-[0.5em]"
                                 />
                             </span>
+                            <div>
+                                <RarityTag
+                                    rarity={it?.Rarity || Rarity.COMMON}
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
