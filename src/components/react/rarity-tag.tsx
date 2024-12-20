f50269d70efd0e3cd739033049e6454ad5c341b2
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2024-12-20 04:09:51 +0100

    Rarity tag coloring

--- src/components/react/rarity-tag.tsx
+++ src/components/react/rarity-tag.tsx
@@ -1,6 +1,6 @@
 import {
-    getMainColorRarity,
     getEdgeColorRarity,
+    getMainColorRarity,
     type Rarity,
 } from "@/gambling/enums";
 
@@ -14,13 +14,15 @@ function invertHexColor(hex: string) {
 }
 
 export const RarityTag: React.FC<{ rarity: Rarity }> = ({ rarity }) => {
-    const color = getMainColorRarity(rarity);
+    const ec = getEdgeColorRarity(rarity);
+    const mc = getMainColorRarity(rarity);
     return (
         <div
             className={`rounded relative leading-4 pt-[0.1em] px-[0.4em] bg-secondary text-white inline-block text-nowrap`}
             style={{
                 clipPath: "border-box",
-                border: `3px solid ${color}`,
+                border: `3px solid ${mc}`,
+                // backgroundColor: mc,
                 fontWeight: "lighter",
             }}
         >
@@ -31,7 +33,7 @@ export const RarityTag: React.FC<{ rarity: Rarity }> = ({ rarity }) => {
                         "linear-gradient(-45deg, #00000000 40%, #fafafa 50%, #00000000 60%)",
                 }}
             />
-            {rarity}
+            <span className="text-white">{rarity}</span>
         </div>
     );
 };
