4f90e173c8fce594c760025a792566b89efb3047
Tobias Kendlbacher <kendlbat@edu.htl-villach.at>
2024-12-20 09:24:23 +0100

    Fix insert coins

--- src/pages/api/users/[userId]/coins.ts
+++ src/pages/api/users/[userId]/coins.ts
@@ -4,28 +4,47 @@ import type { APIRoute } from "astro";
 import { getSession } from "auth-astro/server";
 import { eq, sql } from "drizzle-orm";
 
-export const POST: APIRoute = async ({request, params, cookies}) => {
+export const POST: APIRoute = async ({ request, params, cookies }) => {
     const session = await getSession(request);
     if (!session?.user?.email) {
-        return new Response("Unauthorized", {status: 401});
+        return new Response("Unauthorized", { status: 401 });
     }
 
     const user = params.userId;
 
     if (session.user.email != user) {
-        return new Response("Forbidden", {status: 403});
+        return new Response("Forbidden", { status: 403 });
     }
 
-    const userBalance = await db.select().from(balance).where(sql`${balance.user} = ${user}`);
-    if (userBalance.length != 1) {
-        return new Response("Not found", { status: 404});
+    let userBalance =
+        (
+            await db
+                .select()
+                .from(balance)
+                .where(sql`${balance.user} = ${user}`)
+        )[0]?.amount || -1;
+
+    if (userBalance === -1) {
+        await db.insert(balance).values({
+            user,
+            amount: 0,
+        });
+        userBalance = 0;
     }
 
     const input = (await request.json()) as { amount: number };
 
-    const amount = userBalance[0].amount + input.amount;
+    const amount = userBalance + input.amount;
 
-    await db.update(balance).set({ amount: amount }).where(sql`${balance.user} = ${user}`);
+    await db
+        .update(balance)
+        .set({ amount: amount })
+        .where(sql`${balance.user} = ${user}`);
 
-    return new Response(JSON.stringify({ ...userBalance[0], amount}), {status: 200})
-}
+    return new Response(
+        JSON.stringify({ amount, user, name: session?.user?.name || "" }),
+        {
+            status: 200,
+        }
+    );
+};
